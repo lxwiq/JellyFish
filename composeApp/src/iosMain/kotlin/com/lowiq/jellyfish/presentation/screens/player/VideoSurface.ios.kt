c543e049eecf6114db95ba9560aec901bffece0c
lxwiq <loic.aviez91@gmail.com>
2025-12-19 19:48:42 +0100

    feat(player): add JVM and iOS VideoSurface stubs

---
+++ composeApp/src/iosMain/kotlin/com/lowiq/jellyfish/presentation/screens/player/VideoSurface.ios.kt
@@ -0,0 +1,26 @@
+package com.lowiq.jellyfish.presentation.screens.player
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.lowiq.jellyfish.domain.player.VideoPlayer
+
+@Composable
+actual fun VideoSurface(
+    videoPlayer: VideoPlayer,
+    modifier: Modifier
+) {
+    Box(
+        modifier = modifier.background(Color.Black),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "iOS video playback not yet implemented",
+            color = Color.White
+        )
+    }
+}
