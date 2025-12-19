54353dba8cd9c4c3ac9068807637ad3581d74b79
lxwiq <loic.aviez91@gmail.com>
2025-12-19 23:15:28 +0100

    feat(download): register download components in Koin DI

--- composeApp/src/androidMain/kotlin/com/lowiq/jellyfish/di/PlatformModule.android.kt
+++ composeApp/src/androidMain/kotlin/com/lowiq/jellyfish/di/PlatformModule.android.kt
@@ -1,11 +1,14 @@
 package com.lowiq.jellyfish.di
 
 import com.lowiq.jellyfish.data.local.DATA_STORE_FILE_NAME
+import com.lowiq.jellyfish.data.local.FileManager
 import com.lowiq.jellyfish.data.local.SecureStorage
 import com.lowiq.jellyfish.data.local.createDataStore
 import com.lowiq.jellyfish.data.remote.JellyfinDataSource
 import com.lowiq.jellyfish.data.remote.JellyfinDataSourceImpl
 import com.lowiq.jellyfish.domain.player.VideoPlayer
+import io.ktor.client.*
+import io.ktor.client.engine.okhttp.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 import org.koin.dsl.module
@@ -18,4 +21,6 @@ actual val platformModule: Module = module {
     }
     single<JellyfinDataSource> { JellyfinDataSourceImpl(androidContext()) }
     factory { VideoPlayer(androidContext()) }
+    single { HttpClient(OkHttp) }
+    single { FileManager(androidContext()) }
 }
--- composeApp/src/jvmMain/kotlin/com/lowiq/jellyfish/di/PlatformModule.jvm.kt
+++ composeApp/src/jvmMain/kotlin/com/lowiq/jellyfish/di/PlatformModule.jvm.kt
@@ -1,11 +1,14 @@
 package com.lowiq.jellyfish.di
 
 import com.lowiq.jellyfish.data.local.DATA_STORE_FILE_NAME
+import com.lowiq.jellyfish.data.local.FileManager
 import com.lowiq.jellyfish.data.local.SecureStorage
 import com.lowiq.jellyfish.data.local.createDataStore
 import com.lowiq.jellyfish.data.remote.JellyfinDataSource
 import com.lowiq.jellyfish.data.remote.JellyfinDataSourceImpl
 import com.lowiq.jellyfish.domain.player.VideoPlayer
+import io.ktor.client.*
+import io.ktor.client.engine.okhttp.*
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import java.io.File
@@ -19,4 +22,6 @@ actual val platformModule: Module = module {
     }
     single<JellyfinDataSource> { JellyfinDataSourceImpl() }
     factory { VideoPlayer() }
+    single { HttpClient(OkHttp) }
+    single { FileManager() }
 }
