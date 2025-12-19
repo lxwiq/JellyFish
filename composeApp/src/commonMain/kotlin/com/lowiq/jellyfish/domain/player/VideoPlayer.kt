48b54af61ecff06f025ac8d8047342d4fdb53378
lxwiq <loic.aviez91@gmail.com>
2025-12-19 19:32:21 +0100

    feat(player): add VideoPlayer expect class interface

---
+++ composeApp/src/commonMain/kotlin/com/lowiq/jellyfish/domain/player/VideoPlayer.kt
@@ -0,0 +1,28 @@
+package com.lowiq.jellyfish.domain.player
+
+import kotlinx.coroutines.flow.StateFlow
+
+expect class VideoPlayer {
+    val playbackState: StateFlow<PlaybackState>
+    val audioTracks: StateFlow<List<AudioTrack>>
+    val subtitleTracks: StateFlow<List<SubtitleTrack>>
+    val qualityOptions: StateFlow<List<QualityOption>>
+
+    fun initialize()
+    fun release()
+
+    fun play(url: String, headers: Map<String, String> = emptyMap(), startPositionMs: Long = 0)
+    fun pause()
+    fun resume()
+    fun stop()
+
+    fun seekTo(positionMs: Long)
+    fun seekForward(ms: Long = 10_000)
+    fun seekBackward(ms: Long = 10_000)
+
+    fun setPlaybackSpeed(speed: Float)
+    fun selectAudioTrack(index: Int)
+    fun selectSubtitleTrack(index: Int)
+    fun disableSubtitles()
+    fun selectQuality(index: Int)
+}
